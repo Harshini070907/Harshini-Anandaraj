5438f51b818af13cb7930eb14202e4bfc21ed669
Harshini070907 <nayaroo190309@gmail.com>
2026-04-29 01:10:11 +0800

    added file processing feature

--- question-evaluator/app.py
+++ question-evaluator/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from docx import Document
+from modules.classifier import classify_questions_list
 from modules.classifier import classify_question
 
 app = Flask(__name__)
@@ -19,6 +21,29 @@ def home():
 
     return render_template("index.html")
 
+def read_docx(file_path):
+    doc = Document(file_path)
+    questions = []
+
+    for para in doc.paragraphs:
+        text = para.text.strip()
+        if text:
+            questions.append(text)
+
+    return questions
+
+@app.route("/process-all")
+def process_all():
+    intro_questions = read_docx("intro.docx")
+    networking_questions = read_docx("networking.docx")
+
+    intro_result = classify_questions_list(intro_questions)
+    networking_result = classify_questions_list(networking_questions)
+
+    return {
+        "Intro to Computer": intro_result,
+        "Networking": networking_result
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
